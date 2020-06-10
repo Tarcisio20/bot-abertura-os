559363059f706e407ae61d5dc2585e0be8d8a903
Tarcisio Silva <tarcisior19@hotmail.com>
2020-06-10 19:24:19 -0300

    Acesso a pagina e garimpo de informações uteis

--- robots/robot_put.js
+++ robots/robot_put.js
@@ -3,10 +3,14 @@ const puppeteer = require('puppeteer')
 const robot_put = async () => {
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
-    await page.goto('https://example.com');
-    await page.screenshot({path: 'example.png'});
+    await page.goto('http://10.85.0.88/backoffice/login.jsf');
+    await page.waitFor(1000)
+    //await page.screenshot({path: 'example.png'});
+    // Input Login => formulario:txLoginInput
+    // Input Senha => formulario:txSenhaInput
+    // Button      => formulario:botaoEntrar
   
-    await browser.close();
+   // await browser.close();
 }
 
 module.exports = robot_put
