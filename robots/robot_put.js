64f46bd88fb2dce1d82415880982b71e4738cca1
Tarcisio Silva <tarcisior19@hotmail.com>
2020-06-11 15:04:13 -0300

    feat : Resolução para limpar erro ao entrar na pagina

--- robots/robot_put.js
+++ robots/robot_put.js
@@ -1,14 +1,34 @@
 const puppeteer = require('puppeteer')
 
-const robot_put = async () => {
+const variables = require('../configs.json')
+
+const robot_put = async ( params ) => {
     const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
-    await page.goto('http://10.85.0.88/backoffice/login.jsf');
+    // waitUntil => espera a pagina carregar por completo
+    await page.goto(variables.URL);
+    await page.waitFor(3000);
+    //await page.click('.ui-widget-content > table');
+    await page.keyboard.press(String.fromCharCode(27))
+    await page.waitFor(3000);
+   /* await page.evaluate(()=> {
+       console.log(document.querySelector('.ui-dialog-content > table'));
+    })*/
+    await page.waitFor('input[name="formulario:txLoginInput"]')
     await page.waitFor(1000)
+    await page.type('input[name="formulario:txLoginInput"]', variables.LOGIN , { delay:300 })
+    await page.waitFor(1000)
+
+    await page.type('input[name="formulario:txSenhaInput"]', variables.PASS , { delay:300 })
+    await page.waitFor(1000)
+    await page.keyboard.press('Enter')
+    //await page.goto(variables.URL)
     //await page.screenshot({path: 'example.png'});
     // Input Login => formulario:txLoginInput
     // Input Senha => formulario:txSenhaInput
     // Button      => formulario:botaoEntrar
+    // Mensagem de Erro => messages:messagesId
+    // .ui-dialog-content.ui-widget-content
   
    // await browser.close();
 }
